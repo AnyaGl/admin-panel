d0b73615b4b153ad7788acfbba01c532411c79ef
AnyaGl <gladysheva_2000@inbox.ru>
2022-04-10 17:52:27 +0300

    Сделана генерация кода виджета

--- src/api/product/content-types/product/lifecycles.js
+++ src/api/product/content-types/product/lifecycles.js
@@ -15,17 +15,32 @@ function createProductJson(data) {
   };
 }
 
-module.exports = { 
+function getWidjetCode(id) {
+  return `<div class="saas_shop_tariffs" data-product_id="${id}"></div>
+<script>
+  saas_shop_create_tariffs(${id});
+</script>`
+}
+
+module.exports = {
+
   async afterCreate(event) {
     const product = createProductJson(event.result);
     const broker = await rabbit.getInstance();
     await broker.send('product_service:product.created', Buffer.from(JSON.stringify(product)));
+    needSendUpdateMsg = false;
+    await strapi.query('api::product.product').update({where: {id: product.product.id}, data: {widjet: getWidjetCode(product.product.id)}});
+
   },
 
   async afterUpdate(event) {
-    const product = createProductJson(event.result);
-    const broker = await rabbit.getInstance();
-    await broker.send('product_service:product.updated', Buffer.from(JSON.stringify(product)));
+    if (needSendUpdateMsg)
+    {
+      const product = createProductJson(event.result);
+      const broker = await rabbit.getInstance();
+      await broker.send('product_service:product.updated', Buffer.from(JSON.stringify(product)));
+    }
+    needSendUpdateMsg = true;
   }, 
 
   async afterDelete(event) {
