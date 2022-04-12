2371683dbf9074599f58c237fddef58777ed8954
NextGenen <32591950+gstaizer@users.noreply.github.com>
2022-04-12 12:11:27 +0300

    upd database and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ WORKDIR /app
 COPY . .
 RUN npm install
 RUN npm run build
+RUN db_dump.sql
 EXPOSE 6540
 CMD ["npm", "run", "start"] 
--- config/database - Local.js
+++ config/database - Local.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 6540),
-      database: env('DATABASE_NAME', 'main_db'),
-      user: env('DATABASE_USERNAME', 'main_db'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'product'),
+      user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', ''),
       schema: env('DATABASE_SCHEMA', 'product'),
       ssl: env.bool('DATABASE_SSL', false),
