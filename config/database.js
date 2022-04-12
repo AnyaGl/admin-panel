cc084d92b089c82ebba110dc86e4674ac2ce6323
NextGenen <32591950+gstaizer@users.noreply.github.com>
2022-04-12 12:54:42 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '185.91.52.146'),
+      host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'main_db'),
       user: env('DATABASE_USERNAME', 'main_db'),
