439699158e19e2e2802a08cf28448a44aacf4fdc
NextGenen <32591950+gstaizer@users.noreply.github.com>
2022-04-12 12:48:46 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', '185.91.52.146'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'main_db'),
       user: env('DATABASE_USERNAME', 'main_db'),
-      password: env('DATABASE_PASSWORD', '${{ secrets.PASSWORD }}'),
+      password: env('DATABASE_PASSWORD', '1'),
       schema: env('DATABASE_SCHEMA', 'product'),
       ssl: env.bool('DATABASE_SSL', false),
     },
