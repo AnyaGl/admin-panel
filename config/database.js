54a57746cb3f78d06542e9e355c32532158a8bab
gstaizer <32591950+gstaizer@users.noreply.github.com>
2022-04-14 12:41:54 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'main_db'),
-      user: env('DATABASE_USERNAME', 'main_db'),
-      password: env('DATABASE_PASSWORD', '1'),
+      database: env('DATABASE_NAME', 'product'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', ''),
       schema: env('DATABASE_SCHEMA', 'product'),
       ssl: env.bool('DATABASE_SSL', false),
     },
